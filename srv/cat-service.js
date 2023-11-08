d8e5c7370f89582b820b753d90d7eb4f7da235f6
Saurav Chetry <saurav.chetry@sap.com>
2023-11-08 13:13:21 +0000

    minor changes

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -1,3 +1,5 @@
+// JavaScript Code Sample from API documentation used for this implementation
+
 const { default: cds } = require('@sap/cds');
 const XMLHttpRequest = require('xhr2');
 
@@ -9,57 +11,44 @@ const xhr = new XMLHttpRequest();
 
 // Set up the request
 xhr.open('GET', apiUrl, true);
-
-//adding request headers
-xhr.setRequestHeader("X-ARIBA-NETWORK-ID", "AN02000000280");
-//API Key for API Sandbox
-xhr.setRequestHeader("APIKey", "dMqII3LaXJ383kLD6qrzASvqpi6TlgMa");
+//Add request headers
+xhr.setRequestHeader("X-ARIBA-NETWORK-ID", "AN02000000280"); // ANID from step 1
+xhr.setRequestHeader("APIKey", "dMqII3LaXJ383kLD6qrzASvqpi6TlgMa"); // API Key from step 1
 xhr.setRequestHeader("DataServiceVersion", "2.0");
 xhr.setRequestHeader("Accept", "application/json");
 
-// Define a callback for when the request completes
+// Handle the response
 xhr.onload = async function () {
   if (xhr.status === 200) {
-
     const service = await cds.connect.to("CatalogService");
-    const   { Orders } = service.entities;
-
+    const { Orders } = service.entities;
     // Parse the response as JSON
     const apiResponse = JSON.parse(xhr.responseText);
 
-
     // Loop through each item in the 'content'
     apiResponse.content.forEach(async item => {
+      const write2HC = UPSERT.into(Orders).entries(
+        {
+          vendorId: item.vendorId,
+          customerName: item.customerName,
+          supplierName: item.supplierName,
+          documentNumber: item.documentNumber,
 
-        const write2HC = UPSERT.into(Orders).entries(
-
-            {
-                vendorId : item.vendorId,
-                customerName : item.customerName,
-                supplierName : item.supplierName,
-                documentNumber : item.documentNumber,
-
-            })
-
-        try{
-            const orders = await service.run(write2HC);
-            console.log(orders);
-
-        }catch(error){
-            console.log(error);
-        }
-
-
+        })
+      try {
+        const orders = await service.run(write2HC);
+        console.log(orders);
+      } catch (error) {
+        console.log(error);
+      }
     });
   } else {
     console.log('API request failed with status:', xhr.status);
   }
 };
-
 // Handle errors
 xhr.onerror = function () {
   console.error('Error making API request');
 };
-
 // Send the request
 xhr.send();
